cc471d458f9da2c6f41b0a12c9c57f423be846ca
jusebandtec <jose.silva@bandtec.com.br>
2020-12-17 23:29:05 -0300

    adicionando peças

--- js/pecas.js
+++ js/pecas.js
@@ -4,24 +4,31 @@ function Peca() {
     this.valor
 }
 
-const peao = new Peca()
-const torre = new Peca()
 
 // Criação da peça peão
+const peao = new Peca()
 peao.nome = 'Peão'
 peao.imgPeca = 'https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bp.png'
 peao.valor = 1
 
-nomePecaPeao.innerHTML = peao.nome
-imgPecaPeao.src = peao.imgPeca
-valorPecaPeao.innerHTML = peao.valor
+// nomePecaPeao.innerHTML = peao.nome
+// imgPecaPeao.src = peao.imgPeca
+// valorPecaPeao.innerHTML = peao.valor
 
 
 // Criação da peça Torre
+const torre = new Peca()
 torre.nome = 'Torre'
 torre.imgPeca = 'https://images.chesscomfiles.com/chess-themes/pieces/neo/150/br.png'
 torre.valor = 2
 
-nomePecaTorre.innerHTML = torre.nome
-imgPecaPeao.src = torre.imgPeca
-valorPecaPeao.innerHTML = torre.valor
+// nomePecaTorre.innerHTML = torre.nome
+// imgPecaPeao.src = torre.imgPeca
+// valorPecaPeao.innerHTML = torre.valor
+const bispo = new Peca()
+bispo.nome = 'Bispo'
+bispo.imgPeca = 'https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bb.png'
+
+const cavalo = new Peca()
+cavalo.nome = 'Cavalo'
+cavalo.imgPeca = 'https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bn.png'
